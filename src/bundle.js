3f07e672e294280d139a985288593cc28392d129
benton <ben@benten.ca>
2024-12-28 02:48:54 -0700

    lol

--- src/bundle.js
+++ src/bundle.js
@@ -2,5 +2,6 @@ import Alpine from "../assets/alpinejs@3.14.7.js";
 import Blackberry from "./blackberry.js";
 export default Blackberry;
 window.Alpine = Alpine;
+window.Blackberry = Blackberry;
 Alpine.plugin(Blackberry());
 Alpine.start();
